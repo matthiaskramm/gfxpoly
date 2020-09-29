1521007fd6523be956526a0f36c5fa31aec9d3c7
Matthias Kramm <kramm@quiss.org>
2020-09-29 09:21:11 -0700

    add Python binding scaffolding

---
+++ python/gfxpoly.c
@@ -0,0 +1,149 @@
+#include <Python.h>
+
+#include "gfxpoly.h"
+
+// Opaque container for polygons
+extern PyTypeObject PyGfxPoly;
+
+// The canvas utility, for creating polygons
+extern PyTypeObject PyGfxCanvas;
+
+// String constants for tp_getattro. Initialized on module initialization.
+static PyObject* k_gridsize;
+static PyObject* k_id;
+
+typedef struct {
+    PyObject_HEAD
+    gfxpoly_t* poly;
+} PyGfxPolyObj;
+
+typedef struct {
+    PyObject_HEAD
+    gfxcanvas_t* canvas;
+} PyGfxCanvasObj;
+
+static PyObject* WrapGfxPoly(gfxpoly_t* poly) {
+    PyObject* obj = _PyObject_New(&PyGfxPoly);
+    PyGfxPolyObj* poly_obj = (PyGfxPolyObj*)(obj);
+    poly_obj->poly = poly;
+    return obj;
+}
+
+static PyObject* GfxPolyNew(PyTypeObject* type,
+                            PyObject* args, PyObject* kwargs) {
+    double gridsize;
+    if (!PyArg_ParseTuple(args, "f", &gridsize))
+        return NULL;
+    gfxpoly_t* poly = gfxpoly_from_fill(NULL, gridsize);
+    return WrapGfxPoly(poly);
+}
+
+static PyObject* GfxPolyGetAttro(PyObject* self, PyObject* attr) {
+    Py_RETURN_NONE;
+}
+
+static int GfxPolySetAttro(PyObject* self, PyObject* attr, PyObject* val) {
+    return -1;
+}
+
+static PyObject* GfxPolyRepr(PyObject* self) {
+    PyGfxPolyObj* poly_obj = (PyGfxPolyObj*)(self);
+    PyObject* str = PyUnicode_FromFormat(
+            "<gfxpoly %f", poly_obj->poly->gridsize);
+    PyObject* final_str = PyUnicode_Concat(str, PyUnicode_FromString(">"));
+    Py_DECREF(str);
+    str = final_str;
+    return str;
+}
+
+static void GfxPolyDealloc(PyObject* self) {
+    PyGfxPolyObj* poly = (PyGfxPolyObj*)(self);
+    gfxpoly_destroy(poly->poly);
+    PyObject_Del(self);
+}
+
+static PyMethodDef gfxpoly_methods[] = {
+    //{"NewPoint", (PyCFunction)(NewPoint),
+    //    METH_VARARGS|METH_KEYWORDS, new_point_doc},
+    {0, 0, 0, NULL}    // sentinel
+};
+
+PyDoc_STRVAR(
+    gfxpoly_doc,
+    "Opaque container for polygons\n"
+);
+
+PyTypeObject PyGfxPoly = {
+    PyVarObject_HEAD_INIT(&PyType_Type, 0)
+    tp_name : "GfxPoly",
+    tp_basicsize : sizeof(PyGfxPolyObj),
+    tp_itemsize : 0,
+    tp_dealloc : GfxPolyDealloc,
+    tp_vectorcall_offset: -1,
+    tp_getattr : NULL,
+    tp_setattr : NULL,
+    tp_as_async : NULL,
+    tp_repr : GfxPolyRepr,
+    tp_as_number : NULL,
+    tp_as_sequence : NULL,
+    tp_as_mapping : NULL,
+    tp_hash : NULL,
+    tp_call : NULL,
+    tp_str : NULL,
+    tp_getattro : GfxPolyGetAttro,
+    tp_setattro : GfxPolySetAttro,
+    tp_as_buffer : NULL,
+    tp_flags : 0,
+    tp_doc : gfxpoly_doc,
+    tp_traverse : NULL,
+    tp_clear : NULL,
+    tp_richcompare : NULL,
+    tp_weaklistoffset : 0,
+    tp_iter : NULL,
+    tp_iternext : NULL,
+    tp_methods : gfxpoly_methods,
+    tp_members : NULL,
+    tp_getset : NULL,
+    tp_base : NULL,
+    tp_dict : NULL,
+    tp_descr_get : NULL,
+    tp_descr_set : NULL,
+    tp_dictoffset : 0,
+    tp_init : NULL,
+    tp_alloc : NULL,
+    tp_new : GfxPolyNew,
+};
+
+// --- module -----------------------------------------------------------------
+
+PyDoc_STRVAR(module_doc,
+    "gfxpoly is a polygon intersection engine\n");
+
+static PyMethodDef module_methods[] = {
+    {0, 0, 0, NULL},    // sentinel
+};
+
+static struct PyModuleDef gfxpoly_moduledef = {
+    PyModuleDef_HEAD_INIT,
+    m_name : "gfxpoly",
+    m_doc : module_doc,
+    m_size : -1,
+    m_methods : module_methods,
+    m_slots : NULL,
+    m_traverse : NULL,
+    m_clear : NULL,
+    m_free : NULL,
+};
+
+PyMODINIT_FUNC PyInit_gfxpoly(void) {
+    PyObject* module = PyModule_Create(&gfxpoly_moduledef);
+    PyObject* module_dict = PyModule_GetDict(module);
+
+    PyDict_SetItemString(module_dict, "GfxPoly", (PyObject*)(&PyGfxPoly));
+
+    Py_XDECREF(k_gridsize);
+    k_gridsize = PyUnicode_FromString("gridsize");
+    Py_XDECREF(k_id);
+    k_id = PyUnicode_FromString("id");
+    return module;
+}
